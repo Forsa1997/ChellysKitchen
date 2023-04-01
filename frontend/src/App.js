2490f585a398dbd5e75afeed5bc8c57cf28e00d3
Christoph Ruhe <Christoph-Ruhe@web.de>
2023-04-01 16:33:16 +0200

    did some work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import Album from './pages/MainPage';
+import Navbar from './modules/views/Navbar';
+import Footer from './modules/views/Footer';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { Routes, Route } from 'react-router-dom';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Album />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/terms" element={<Privacy />} />
+        <Route path="/privacy" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import withRoot from '../modules/withRoot';
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const theme = createTheme();
+
+const MainPage = () => {
+    return (
+        // <ThemeProvider theme={theme}>
+        <>
+            <CssBaseline />
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    pt: 8,
+                    pb: 6,
+                }}
+            >
+                <Container maxWidth="sm">
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Recipes
+                    </Typography>
+                    <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        At vero eos et accusam et justo duo dolores et ea rebum.
+                    </Typography>
+                    <Stack
+                        sx={{ pt: 4 }}
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center"
+                    >
+                        <Button variant="contained">Create Recipe</Button>
+                        <Button variant="outlined">Secondary action</Button>
+                    </Stack>
+                </Container>
+            </Box>
+            <Container sx={{ py: 8 }}>
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                            >
+                                <CardMedia
+                                    component="img"
+                                    sx={{
+                                        // 16:9
+                                    }}
+                                    image="https://picsum.photos/900/600"
+                                    alt="random"
+                                />
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Recipe
+                                    </Typography>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                        At vero eos et accusam et justo duo dolores et ea rebum.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">View</Button>
+                                    <Button size="small">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </>
+        // </ThemeProvider>
+    );
+
+}
+
+export default withRoot(MainPage);
