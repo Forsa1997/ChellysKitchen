6a8b0b68b0e873b759e984cbfb5676eb74477d4f
Christoph Ruhe <Christoph-Ruhe@web.de>
2025-03-14 21:45:47 +0100

    Add template files to the project

---
+++ frontend/src/components/SitemarkIcon.tsx
@@ -0,0 +1,17 @@
+import SvgIcon from '@mui/material/SvgIcon';
+
+export default function SitemarkIcon() {
+    return (
+        <SvgIcon sx={{ height: 40, width: 200, mr: 2 }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 100" width="200" height="100">
+                <text x="10" y="70" font-family="Brush Script MT, cursive" font-size="80" fill="#f2a7b9">Chellys Kitchen</text>
+            </svg>
+
+
+
+
+
+
+        </SvgIcon>
+    );
+}
